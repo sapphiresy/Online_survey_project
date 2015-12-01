6341cc69dbe5bd908e42398366c42f904bfd488a
sapphire- <sooyeon.song22@gmail.com>
2015-12-01 14:50:14 +0900

    add
    user model

--- app.js
+++ app.js
@@ -32,7 +32,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'long-long-long-secret-string-1313513tefgwdsvbjkvasd'
+}));
+app.use(flash());
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
