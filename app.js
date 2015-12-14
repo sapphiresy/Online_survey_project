06bb4a7b7a3ed9225240e9bf1dce6d90ef2a6e89
sapphire- <sooyeon.song22@gmail.com>
2015-12-15 00:07:33 +0900

    facebook login

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ var session = require('express-session');
 var methodOverride = require('method-override');
 var flash = require('connect-flash');
 var mongoose   = require('mongoose');
+var passport = require('passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var configAuth = require('./config/auth');
+var routeAuth = require('./routes/auth');
 
 var app = express();
 
@@ -35,24 +38,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
 
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: 'long-long-long-secret-string-1313513tefgwdsvbjkvasd'
-}));
-app.use(flash());
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(function(req, res, next) {
-  res.locals.currentUser = req.session.user;
+  console.log("REQ USER", req.user);
+  res.locals.currentUser = req.user;
   res.locals.flashMessages = req.flash();
   next();
 });
 
+configAuth(passport);
+
 app.use('/', routes);
 app.use('/users', users);
+routeAuth(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
